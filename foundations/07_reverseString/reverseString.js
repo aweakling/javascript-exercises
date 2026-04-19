ced7c9019ac1573a89d732a521fa2bb6e39e3781
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-04-19 14:45:12 +0800

    Finish 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+function reverseString(str){
 
-};
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+console.log(reverseString("I am Michelangelo Apita"));
 
 // Do not edit below this line
 module.exports = reverseString;
