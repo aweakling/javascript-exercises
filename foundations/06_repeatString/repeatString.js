e2e5dda1e602a0d617d8ae1785272d138dd18ceb
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-04-19 14:37:16 +0800

    Finish 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+function repeatString(string, num){
 
-};
+    let result = '';
+        if(num > 0){
+            for(let i = 0; i < num; i++){
+                result += string;
+            }
+        } else {
+            return "ERROR";
+        }
+    return result;
+}
 
+console.log(repeatString("hey",3));
 // Do not edit below this line
 module.exports = repeatString;
