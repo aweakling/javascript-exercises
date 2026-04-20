9f4ce0f85229ef0561541a1cd40fbdf76d2f45d1
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-04-20 11:22:56 +0800

    Finish 08,09,10,11 exercises

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, itemToRemove) {
+    
+    return array.filter(item => item !== itemToRemove);
 };
+
+let array = [1, 2, 3, 4];
+
+console.log(removeFromArray(array, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let convert = (5 / 9) * (temp - 32);
+
+  return convert.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  let convert = (temp * 9 / 5) + 32;
+
+  return convert.toFixed(1);
 };
 
 // Do not edit below this line
